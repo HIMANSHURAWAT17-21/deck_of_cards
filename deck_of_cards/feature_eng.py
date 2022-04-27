b4de3908766a373ddb0d72cf4b309590a9698159
unknown <himanshurawat05062000@gmail.com>
2022-04-27 09:22:30 +0530

    first commit

---
+++ deck_of_cards/feature_eng.py
@@ -0,0 +1,10 @@
+
+
+# Comes from 00_core.ipynb, cell
+ print("all the feature eng stuff here ")
+
+# Comes from 00_core.ipynb, cell
+ print("all the feature eng stuff here ")
+
+# Comes from 00_core.ipynb, cell
+print("another cell to feature_eng")
---
+++ deck_of_cards/training.py
@@ -0,0 +1,7 @@
+
+
+# Comes from 00_core.ipynb, cell
+print("training here")
+
+# Comes from 00_core.ipynb, cell
+print("training here")
